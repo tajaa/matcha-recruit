801106fabd936f9ed7db14062058a1c2fc9be2c7
finch <tajatheprince@gmail.com>
2026-01-13 18:30:43 -0600

    fix: Set S3 objects to public-read for CloudFront access

--- server/app/services/storage.py
+++ server/app/services/storage.py
@@ -70,6 +70,7 @@ class StorageService:
                     Bucket=self.bucket,
                     Key=key,
                     Body=file_bytes,
+                    ACL='public-read',
                     **extra_args,
                 )
                 return self._get_cloudfront_url(key)
